9358654abd5573a3883419590ed6093937315381
Jaydon Goodrich <jaydongoodrich@gmail.com>
2021-03-23 16:23:04 -0600

    db connection and server Hello World

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('HELLO WORLD');
+})
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}!`);
+    });
+});
+
